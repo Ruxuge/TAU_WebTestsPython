5e8d26182510e46833dbef2db16a70b12122c6d7
Ruxuge <filipwerra.12@gmail.com>
2021-03-18 15:48:57 +0100

    firefox tests ready

--- firefox/pudelek/donos.py
+++ firefox/pudelek/donos.py
@@ -9,6 +9,6 @@ def send_correct_text_donos(driver):
     element = driver.find_element_by_css_selector('.sc-2rdf5u-3')
     element.send_keys("Nowy donos testowy")
     time.sleep(10)
-    element = driver.find_element_by_xpath('/html/body/div[3]/div/div[4]/div[1]/div/div[2]/div/div/div/div[3]/form/div[8]/span')
-    element.click()
+    #element = driver.find_element_by_xpath('/html/body/div[3]/div/div[4]/div[1]/div/div[2]/div/div/div/div[3]/form/div[8]/span')
+    #element.click()
 
